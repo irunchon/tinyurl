0e166ff5134fcebb9263d866a73a0378a423391d
Irina <irunchon@Irinas-MacBook-Air.local>
2023-06-27 16:37:27 +0300

    +log for log level parsing error

--- internal/pkg/logger/logger.go
+++ internal/pkg/logger/logger.go
@@ -1,6 +1,8 @@
 package logger
 
 import (
+	"fmt"
+
 	"go.uber.org/zap"
 )
 
@@ -10,11 +12,16 @@ func Initialize(logLevel string) {
 	var loggerConfig = zap.NewProductionConfig()
 
 	parsedLogLevel, parsingError := zap.ParseAtomicLevel(logLevel)
-
 	if parsingError == nil {
 		loggerConfig.Level = parsedLogLevel
 	}
 
 	Logger = zap.Must(loggerConfig.Build())
 	defer Logger.Sync()
+
+	if parsingError != nil {
+		Logger.Error(
+			fmt.Sprintf("failed to parse log level: %v (log level set to info)",
+				parsingError))
+	}
 }
